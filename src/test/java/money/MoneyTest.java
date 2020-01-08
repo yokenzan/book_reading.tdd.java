2883ec2a15ed20ee5afafb2c9f77ad38bef8be7b
yokenzan <31175068+yokenzan@users.noreply.github.com>
2020-01-08 17:59:29 +0900

    第14章 学習用テストと回帰テスト

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -60,4 +60,17 @@ public class MoneyTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testReduceMoneyDiffrentcurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
+    }
 }
