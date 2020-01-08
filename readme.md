d1a8799f0d7f8af821b8a4b7a343417df2fbf6bc
yokenzan <31175068+yokenzan@users.noreply.github.com>
2020-01-08 13:30:40 +0900

    第4章 意図を語るテスト

--- readme.md
+++ readme.md
@@ -18,7 +18,7 @@ cd book_reading.tdd.java
 
 - [ ] $5+ 10CHF = $10 (レートが2:1の場合)
 - [x] $5 * 2 = $10
-- [ ] amountをprivateにする
+- [x] amountをprivateにする
 - [x] Dollarの副作用どうする？
 - [ ] Moneyの丸め処理どうする？
 - [x] equals()
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -12,11 +12,9 @@ public class MoneyTest {
     public void testMultiplication() {
         Dollar five = new Dollar(5);
 
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
 
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
